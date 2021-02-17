f20a2fb7eb1252b65d25b7d9869620ea853d2431
marzione00 <marzio.decorato@outlook.it>
2021-02-17 13:08:51 +0100

    PUB

--- Plotter.R
+++ Plotter.R
@@ -1,9 +1,18 @@
 library(readxl)
 library(ggplot2)
 
-Vendite_READY <- read_excel("GitHub/Advanced_Microeconomics_Project/Tabellozze_ISTAT/Vendite_READY.xlsx")
+#Vendite_READY <- read_excel("GitHub/Advanced_Microeconomics_Project/Tabellozze_ISTAT/Vendite_READY.xlsx")
+Vendite_READY <- read_excel("Tabellozze_ISTAT/Vendite_READY.xlsx")
 
 
-ggplot(data=Vendite_READY,aes(x=Data,y=Alimentari))+geom_line(size=2,color = "red")+geom_line(size=2,aes(x=Data,y=NAlimentare),color = "blue")
+Vendite_READY$delta<-Vendite_READY$Alimentari-Vendite_READY$NAlimentare
 
-didreg = lm(y ~ treated + time + did, data = mydata)
+ggplot(data=Vendite_READY,aes(x=Data,y=Alimentari))+geom_line(size=2,color = "red")+geom_line(size=2,aes(x=Data,y=NAlimentare),color = "blue")+
+geom_line(size=2,color = "red")+geom_line(size=2,aes(x=Data,y=delta),color = "green")
+
+
+
+did <-lm(delta ~  Time+ Time2+Time3, data = Vendite_READY)
+summary(did)
+plot(did)
+
