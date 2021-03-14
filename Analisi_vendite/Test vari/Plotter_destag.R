7e6eaa33d3f9906ffc091b5f6e9837a44c0cbc93
marzione00 <marzio.decorato@outlook.it>
2021-03-14 10:26:41 +0100

    PUB

--- Analisi_vendite/Test vari/Plotter_destag.R
+++ Analisi_vendite/Test vari/Plotter_destag.R
@@ -3,10 +3,20 @@ library(ggplot2)
 Data <- read_excel("Dati_destagionalizzati_FULL.xlsx")
 
 ggplot(data=Data,aes(x=Time,y=Alimentari))+geom_line(size=2,color = "red")+geom_line(size=2,aes(x=Time,y=Abbigliamento),color = "blue")+ 
+  geom_vline(xintercept = -4, linetype="dashed", color = "orange", size=1)+ 
+  geom_vline(xintercept = -3, linetype="dashed", color = "red", size=1)+
+  geom_vline(xintercept = -2, linetype="dashed", color = "orange", size=1)+ 
+  geom_vline(xintercept = -1, linetype="dashed", color = "darkgreen", size=1)+ labs(y = "Selling value")+ labs(x = "Time (months)")+theme_bw(base_size = 18)
+
+
+ggplot(data=Data,aes(x=Time,y=Alimentari))+geom_line(size=2,color = "red")+geom_line(size=2,aes(x=Time,y=Abbigliamento),color = "blue")+ 
   geom_vline(xintercept = 1, linetype="dashed", color = "orange", size=1)+ 
   geom_vline(xintercept = 2, linetype="dashed", color = "red", size=1)+
   geom_vline(xintercept = 3, linetype="dashed", color = "orange", size=1)+ 
   geom_vline(xintercept = 4, linetype="dashed", color = "darkgreen", size=1)+ labs(y = "Selling value")+ labs(x = "Time (months)")+theme_bw(base_size = 18)
+
+
+
 
 ggplot(data=Data,aes(x=Time,y=Alimentari))+geom_line(size=2,color = "red")+geom_line(size=2,aes(x=Time,y=Calzature),color = "darkgreen")+ 
   geom_vline(xintercept = 1, linetype="dashed", color = "orange", size=1)+ 
